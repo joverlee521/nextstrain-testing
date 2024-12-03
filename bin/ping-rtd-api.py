4946494fceee2ac11b2c8043966efa8c8c6306e7
Jover Lee <joverlee521@gmail.com>
2024-12-03 15:01:44 -0800

    ping-rtd-api: Add User-Agent

--- bin/ping-rtd-api.py
+++ bin/ping-rtd-api.py
@@ -4,6 +4,8 @@ ua = requests.Session()
 
 API_URL = "https://readthedocs.org/api/v3/"
 
+ua.headers.update({"User-Agent": "curl/7.81.0"})
+
 res = ua.get(API_URL)
 print(res.request.headers)
 res.raise_for_status()
